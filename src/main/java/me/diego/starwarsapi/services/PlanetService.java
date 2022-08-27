758d563f116ef2596166040241602f9708b9af38
Diego <diegoaragao465@gmail.com>
2022-08-27 01:29:24 -0300

    Save method created

--- src/main/java/me/diego/starwarsapi/services/PlanetService.java
+++ src/main/java/me/diego/starwarsapi/services/PlanetService.java
@@ -2,6 +2,7 @@ package me.diego.starwarsapi.services;
 
 import me.diego.starwarsapi.domain.Planet;
 import me.diego.starwarsapi.repositories.PlanetRepository;
+import me.diego.starwarsapi.util.PlanetRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +13,16 @@ public class PlanetService {
     @Autowired
     PlanetRepository planetRepository;
 
+    @Autowired
+    PlanetRequest planetRequest;
+
     public List<Planet> listAll() {
         return planetRepository.findAll();
     }
 
     public Planet save(Planet planet) {
+        planet.setApparitions(planetRequest.getNumberFilms(planet.getName()));
+
         return planetRepository.save(planet);
     }
 }
--- src/main/java/me/diego/starwarsapi/util/PlanetRequest.java
+++ src/main/java/me/diego/starwarsapi/util/PlanetRequest.java
@@ -4,13 +4,17 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+@Component
 public class PlanetRequest {
-    public Integer getNumberFilms() {
+    public Integer getNumberFilms(String name) {
         Mono<String> retrieve = WebClient.create().get()
-                .uri("https://swapi.dev/api/planets?search=Tatooine")
+                .uri("https://swapi.dev/api/planets?search=%s".formatted(name))
                 .retrieve()
                 .bodyToMono(String.class);
 
@@ -20,7 +24,11 @@ public class PlanetRequest {
         JsonElement parse = jsonParser.parse(block);
 
         JsonObject obj = parse.getAsJsonObject();
-        JsonElement planet = obj.get("results").getAsJsonArray().get(0);
+        JsonArray planetResponse = obj.get("results").getAsJsonArray();
+
+        if(planetResponse.size() == 0) return 0;
+
+        JsonElement planet = planetResponse.get(0);
 
         JsonObject asJsonObject = planet.getAsJsonObject();
         JsonArray films = asJsonObject.getAsJsonArray("films");
